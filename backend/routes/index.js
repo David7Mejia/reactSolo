04d5e25d6ec4284cfe72d60e2038531b4410518e
David Mejia <david7mejia@gmail.com>
2021-06-17 23:16:18 -0500

    import react-redux frontend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,14 @@ const router = express.Router();
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
-router.get('/hello/world', function (req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
+
 
 module.exports = router;
+
+/**
+ *
+ router.get('/hello/world', function (req, res) {
+     res.cookie('XSRF-TOKEN', req.csrfToken());
+     res.send('Hello World!');
+ });
+ */
